e530ad95413fc9aa567b211650e0f791867c92fb
AhmedNasr17 <nasr37237@gmail.com>
2026-02-26 06:56:14 +0200

    added .env file for frontend

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use((req, res,next) => {
   res.status(404).json({ message: 'route not found' });
 });
 
-app.use((error, req, res, next) => {
+app.use((error, req, res, next) => {  
   res.status(error.statusCode || 500).json({
     status: error.status || "error",
     message: error.message
